68648aa4acab40324433d7b52a07c86e79662bc6
jllRogen <jllrogen@gmail.com>
2019-05-30 14:28:56 +0800

    djaidfaf

--- app.js
+++ app.js
@@ -20,31 +20,62 @@ app.use( express.static( path.join( __dirname, 'public' ) ) )
 app.use( express.static( path.join( __dirname, '../dist' ) ) )
 
 const allowHeaders = ( ( ) => {
-  let list = [ "content-type", "user_id", "user_ip", "user_name", "token" ]
-  return list.join( ', ' )
+  let list = [
+		'Authorization',
+		"content-type",
+		'Depth',
+		'User-Agent',
+		'X-File-Size',
+		'X-Requested-With',
+		'X-Requested-By',
+		'If-Modified-Since',
+		'X-File-Name',
+		'X-File-Type',
+		'Cache-Control',
+		'Origin',
+		"user_id",
+		"user_ip",
+		"user_name",
+    "token",
+    "lang",
+    "opsource",
+    "sign"
+    ] // content-type,lang,opsource,sign
+  let str = list.join( ',' )
+  console.log( str )
+  return str
 } )( )
 const allowMethods = ( ( ) => {
-  let list = [ "POST", "GET", "DELETE", "put", "OPTIONS", "PATCH" ]
-  return list.join( ", " )
+  let list = [ "POST", "GET", "DELETE", "PUT", "OPTIONS", "PATCH" ]
+  console.log( list.join( "," ) )
+  return list.join( "," )
 } )( )
+
+// var orginList = [
+//     "http://0.0.0.0:2019",
+//     'http://localhost:2019',
+//      'http://192.168.43.222:8080',
+//     "http://www.alibaba.com",
+//     'http://192.168.43.222:2019',
+//   	"http://www.qq.com",
+//   	"http://www.baidu.com"
+//   ]
 // 支持跨域
 app.all( "*", function ( req, res, next ) {
-  // var orginList = [
-  // 	"http://www.zhangpeiyue.com",
-  // 	"http://www.alibaba.com",
-  // 	"http://www.qq.com",
-  // 	"http://www.baidu.com"
-  // ]
-  // if (orginList.includes(req.headers.origin.toLowerCase())) {
-  // 	res.header("Access-Control-Allow-Origin", req.headers.origin);
-  // }
-  res.header( "Access-Control-Allow-Origin", "*" ); // 允许所有域名
-  res.header( "Access-Control-Allow-Headers", allowHeaders ); // 跨域的请求头里面设置的自定义参数
-  res.header( "Access-Control-Allow-Methods", allowMethods );
-  res.header( "Access-Control-Max-Age", 10 ); // 响应保存10秒
-  res.header( "Access-Control-Expose-Headers", "Cache-Control, Content-Language, Content-Type, Last-Modified" )
+
+  let origin = req.headers.origin.toLowerCase( )
+  console.log( origin )
+  res.header( "Access-Control-Allow-Origin", origin );
+
+  // res.header( "Access-Control-Allow-Origin", "*" );
+  res.header( "Access-Control-Allow-Headers", "Authorization,content-type,Depth,User-Agent,X-File-Size,X-Requested-With,X-Requested-By,If-Modified-Since,X-File-Name,X-File-Type,Cache-Control,Origin,user_id,user_ip,user_name,token,lang,opsource,sign" );
+  // res.header( "Access-Control-Allow-Headers", "*" )
+  res.header( "Access-Control-Allow-Methods", "POST,GET,DELETE,PUT,OPTIONS,PATCH" );
+  res.header( "Access-Control-Allow-Credentials", true )
+  res.header( "Access-Control-Max-Age", 200 ); // 响应保存10秒
+  res.header( "Access-Control-Expose-Headers", "Cache-Control,Authorization,Content-Language, Content-Type,Last-Modified" )
   if ( req.method.toLowerCase( ) === 'options' ) { //让options 预检请求快速结束
-    res.send( 200 );
+    res.sendStatus( 200 );
   } else {
     next( );
   }
@@ -53,7 +84,7 @@ app.all( "*", function ( req, res, next ) {
 
 app.use( "/", require( "./routes/index" ) )
 
-app.use( function ( err, req, res, next ) {
+app.use( function ( err, req, res, ) {
   res.locals.message = err.message
   res.locals.error = req.app.get( 'env' ) === 'development' ? err : {}
 
@@ -65,3 +96,13 @@ app.use( function ( err, req, res, next ) {
 } )
 
 module.exports = app
+
+
+
+
+//Access to XMLHttpRequest at 'http://0.0.0.0:8888/lkjz_cms/home' from origin 'http://192.168.43.222:2019' has been blocked by CORS policy: Response to preflight request doesn't pass access control check: The 'Access-Control-Allow-Origin' header has a value 'http://0.0.0.0:2019' that is not equal to the supplied origin.
+//Access to XMLHttpRequest at 'http://0.0.0.0:8888/lkjz_cms/home' from origin 'http://192.168.43.222:2019' has been blocked by CORS policy: Response to preflight request doesn't pass access control check: The 'Access-Control-Allow-Origin' header has a value 'http://192.168.43.222:8080' that is not equal to the supplied origin.
+//Access to XMLHttpRequest at 'http://0.0.0.0:8888/lkjz_cms/home' from origin 'http://192.168.43.222:2019' has been blocked by CORS policy: Response to preflight request doesn't pass access control check: The 'Access-Control-Allow-Origin' header has a value 'http://192.168.43.222:8080' that is not equal to the supplied origin.
+
+// Access to XMLHttpRequest at 'http://0.0.0.0:8888/lkjz_cms/home' from origin 'http://192.168.43.222:2019' has been blocked by CORS policy: Response to preflight request doesn't pass access control check: The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'. The credentials mode of requests initiated by the XMLHttpRequest is controlled by the withCredentials attribute.
+//Access to XMLHttpRequest at 'http://0.0.0.0:8888/lkjz_cms/home' from origin 'http://192.168.43.222:2019' has been blocked by CORS policy: Request header field lang is not allowed by Access-Control-Allow-Headers in preflight response.
